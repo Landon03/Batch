61d5cb36635a0a9e816aeb7dcbd71bdb145e7922
Landon <55545837+Landon03@users.noreply.github.com>
2020-04-08 15:49:30 -0400

    Add files via upload

---
+++ police/dico.bat
@@ -0,0 +1,13 @@
+@echo off
+set file=C:\Users\Landon\Desktop\police\aound.mp3
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.settings.setMode "loop", True
+  echo Sound.Controls.play
+  echo While Sound.playState ^<^> 1
+  echo wscript.sleep 100
+  echo Wend
+)>sound.vbs
+start /min sound.vbs
+
+exit /b
